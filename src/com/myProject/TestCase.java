aa91b2832f621fda386c0fbb27a7341f19282ca3
ElizzF <838331058@qq.com>
2020-09-12 20:27:34 +0800

    更新测试

--- src/com/myProject/TestCase.java
+++ src/com/myProject/TestCase.java
@@ -7,16 +7,22 @@ import java.io.File;
 class TestCase {
 	@Test
 	public void testSimHash() {
-		File f = new File("test");
+		File f = new File("tests");
 		String[] files = f.list();
 		int cnt = 1;
 		for(String file: files) {
-			if(!file.equals("orig.txt")) {
-				System.out.println("开始处理" + file);
-				MainProcess.Process("test/orig.txt", "test/" + file, "ans/ans" + cnt + ".txt");
+			if(!file.equals("orig.txt") && !file.equals("short_orig.txt") && !file.equals("short_orig_other.txt")) {
+				System.out.println("simHash开始处理：" + file);
+				MainProcess.Process("tests/orig.txt", "tests/" + file, "ans/ans" + cnt + ".txt");
 				cnt++;
 			}
 		}
 	}
-
+	
+	@Test
+	public void testCos() { 
+		System.out.println("cos开始处理：" + "tests/short_orig_other.txt");
+		MainProcess.Process("tests/short_orig.txt", "tests/short_orig_other.txt", "ans/short_ans.txt");
+		
+	}
 }
